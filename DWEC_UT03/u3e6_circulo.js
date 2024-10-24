b92c1574b577856b0533276b93cfcb6acb8a4a09
Juanjo <juanjose.s435736@cesurformacion.com>
2024-10-24 08:25:15 +0200

    Ejercicios (u3e6)

--- DWEC_UT03/u3e6_circulo.js
+++ DWEC_UT03/u3e6_circulo.js
@@ -1,11 +1,17 @@
-let Radio = prompt("Introduce el valor del radio");
+let Radio = prompt("Cual quieres que sea el valor del radio??");
 
-let Diametro = Radio * 2;
+let Diametro = Math.floor(Radio * 2);
 
-let Perimetro = 2 * Math.PI * Radio;
+let Perimetro = Math.floor(2 * Math.PI * Radio);
 
-let AreaCirculo = Math.PI * pow(Radio, 2);
+let AreaCirculo = Math.floor(Math.PI * Math.pow(Radio, 2));
 
-let AreaEsfera = 4 * Math.PI * pow(Radio, 2);
+let AreaEsfera = Math.floor(4 * Math.PI * Math.pow(Radio, 2));
 
-let VolumenEsfera = 4/3 * Math.PI * pow(Radio, 3);
+let VolumenEsfera = Math.floor(4/3 * Math.PI * Math.pow(Radio, 3));
+
+alert("Diámetro de la circunferencia: " + Diametro + "\n" +
+    "Perímetro de la circunferencia: " + Perimetro + "\n" +
+    "Área de la circunferencia: " + AreaCirculo + "\n" +
+    "Área de la esfera: " + AreaEsfera + "\n" +
+    "Volumen de la esfera: " + VolumenEsfera);
